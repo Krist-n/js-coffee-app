2aa3ad2a165a0d76b5884df3b12cc4ecbd8da687
Kristen <kristen1286@gmail.com>
2021-03-17 20:02:19 +0000

    making comments to describe our functions purpose
    Co-Authored-By: Kelsi Flatland <kelsi.flatland@gmail.com>

--- static/js/coffeeShop.js
+++ static/js/coffeeShop.js
@@ -1,5 +1,7 @@
 "use strict";
 
+// append to cart-items, adding rows to a table in cart
+//inside the rows of the data of the item's name
 const addItemToCart = (itemName) => {
   $('#cart-items').append(`
     <tr>
@@ -8,11 +10,14 @@ const addItemToCart = (itemName) => {
   `);
 };
 
+// take the cart total and reset it at 0
+// empty out the items in the cart-items object (html table)
 const resetCart = () => {
-  $('#cart-total').html('0.00');
+  $('#cart-total').html('0.00'); // TODO check out later :) 
   $('#cart-items').empty();
 };
-
+//Creating a var to accumulate cart total
+//and incrementing based on item in cart, setting decimal place
 const incrementCartTotal = (price) => {
   const cartTotal = $('#cart-total');
 
@@ -22,13 +27,18 @@ const incrementCartTotal = (price) => {
   cartTotal.html(total.toFixed(2));
 };
 
+// set variable equal to number of coffee sold
+// increase coffeSold by amountSold
+// coffee-sold-counter - convert to coffeeSold
 const incrementCoffeeSold = (amountSold) => {
   let coffeeSold = Number($('#coffee-sold-counter').html());
   coffeeSold += amountSold;
 
   $('#coffee-sold-counter').html(coffeeSold);
 };
-
+//taking in progressval and statusmsg, whatever that is 
+//looking for id tag order-progress changing attribute to progressVal
+//replacing order-status-message with status message
 const setProgressAndStatus = (progressVal, statusMsg) => {
   $('#order-progress').attr('value', progressVal);
   $('#order-status-message').html(statusMsg);
